8b391bb4234e44e243645a9551bc348905c4b3eb
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2024-05-27 14:16:10 +0000

    Fixing worker threads issue for deno build

--- apps/deno-shell/src/entry.ts
+++ apps/deno-shell/src/entry.ts
@@ -4,9 +4,9 @@ import { setWorkerOverrideUrl } from '@wasmin/wasi-js';
 
 export async function main() {
 
-    //setWorkerOverrideUrl('./wasmComponentWorkerThread.js', import.meta.resolve("./wasmComponentWorkerThread.js"));
-    //setWorkerOverrideUrl('./wasmCoreWorkerThread.js', import.meta.resolve("./wasmCoreWorkerThread.js"));
-    //setWorkerOverrideUrl('./wasiWorkerThread.js', import.meta.resolve("./wasiWorkerThread.js"));
+    setWorkerOverrideUrl('./wasmComponentWorkerThread.js', import.meta.resolve("./wasmComponentWorkerThread.js"));
+    setWorkerOverrideUrl('./wasmCoreWorkerThread.js', import.meta.resolve("./wasmCoreWorkerThread.js"));
+    setWorkerOverrideUrl('./wasiWorkerThread.js', import.meta.resolve("./wasiWorkerThread.js"));
 
     await startShell(denofs);
 }
